9909e25293592ec9154b32490824a792a3d05840
Paul Campbell <pcampbell@kemitix.net>
2020-05-14 19:40:02 +0100

    Convert to a proper Command Mode Application (#27)

--- runner/src/main/java/net/kemitix/clover/CloverMain.java
+++ runner/src/main/java/net/kemitix/clover/CloverMain.java
@@ -1,45 +1,19 @@
 package net.kemitix.clover;
 
-import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
 import net.kemitix.clover.service.CloverService;
-import net.kemitix.clover.spi.FatalCloverError;
+import javax.inject.Inject;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import java.util.Optional;
-import java.util.concurrent.Executors;
-import java.util.logging.Logger;
+@QuarkusMain
+public class CloverMain implements QuarkusApplication {
 
-@ApplicationScoped
-public class CloverMain {
+    @Inject
+    CloverService service;
 
-    private static final Logger LOGGER =
-            Logger.getLogger(
-                    CloverMain.class.getName());
-
-    private final CloverService service;
-
-    public CloverMain(final CloverService service) {
-        this.service = service;
+    @Override
+    public int run(String... args) {
+        service.run();
+        return 0;
     }
-
-    void onStart(@Observes final StartupEvent ev) {
-        Executors.newSingleThreadExecutor()
-                .submit(() -> {
-                    try {
-                        service.run();
-                    } catch (final FatalCloverError e) {
-                        LOGGER.severe(e.getMessage());
-                        Optional.ofNullable(e.getCause())
-                                .ifPresent(cause ->
-                                        LOGGER.severe(cause.getMessage()));
-                    } catch (final Exception e) {
-                        LOGGER.severe("Unexpected error");
-                        e.printStackTrace();
-                    } finally {
-                        System.exit(0);
-                    }
-                });
-    }
-
 }
